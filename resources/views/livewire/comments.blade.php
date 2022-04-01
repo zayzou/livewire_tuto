d4e9b4e33b16646ca4f3cb45b11b854d1e8f4e33
SOFFI ZAHIR <soffizahir2016@gmail.com>
2022-04-02 00:16:59 +0100

    add validation message to view

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -9,13 +9,13 @@
                         <input type="text" placeholder="What's on your mind 💬 " id="footer-field" name="footer-field"
                             wire:model='newComment' wire:keydown.enter='store'
                             class="w-full bg-gray-500 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                    </div>
+                        </div>
                     <button type="submit"
-                        class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                        class="inline-flex text-white bg-indigo-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-00 rounded">
                         Add
                     </button>
                 </form>
-
+                @error('newComment') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 @forelse ($comments as $comment)
                     <div class="p-2 w-full mt-8 border border-gray-200 shadow-lg rounded relative">
                         <a class="title-font text-lg font-medium text-gray-200 mb-3 mr-2">{{ $comment->user->name }}</a>
